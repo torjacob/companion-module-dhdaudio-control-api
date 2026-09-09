e17c062b1a109be1ca6b7e45c6da5040ce5382c5
Tor Jacob Neple <tor.jacob.neple@nrk.no>
2026-09-09 12:50:33 +0200

    implement basic mixers structure

---
+++ src/control-api/mixers.ts
@@ -0,0 +1,57 @@
+import * as z from 'zod'
+import type { ModuleInstance } from '../main.js'
+
+export const Mixer = z.object({
+	_lastloadedsnap: z.string().optional().default('-'),
+	_name: z.string().optional().default('-'),
+	//	automix: automixRecord.optional().default({}),
+	//	cleanfeeds: cleanfeedRecord.optional().default({}),
+	//	mutegroups: mutegroupRecord.optional().default({}),
+	//	options: optionsRecord.optional().default({}),
+	//  faders: faderRecord.optional().default({}),
+	//  sourcelist: sourceArray.optional().default([]),
+})
+
+const MixerId = z.string()
+const MixerRecord = z.record(MixerId, Mixer)
+export type MixerRecord = z.infer<typeof MixerRecord>
+
+const ResponseSuccess = z.object({
+	msgID: z.any(),
+	method: z.literal('get'),
+	path: z.string(),
+
+	success: z.literal(true),
+	payload: MixerRecord,
+})
+
+const ResponseError = z.object({
+	msgID: z.any(),
+	method: z.literal('get'),
+	path: z.string(),
+
+	success: z.literal(false),
+	error: z.object({
+		code: z.number(),
+		message: z.string(),
+	}),
+})
+
+const Response = z.union([ResponseSuccess, ResponseError])
+
+export async function fetchMixers(self: ModuleInstance): Promise<MixerRecord> {
+	return new Promise((resolve, reject) => {
+		self.websocket.get('/audio/mixers', (response) => {
+			const result = z.safeParse(Response, response)
+			if (!result.success) {
+				return reject(new Error(result.error.message))
+			}
+
+			if (result.data.success === false) {
+				return reject(new Error(result.data.error.message))
+			}
+
+			return resolve(result.data.payload)
+		})
+	})
+}
